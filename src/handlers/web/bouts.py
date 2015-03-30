05b2c3a29da158070763aad69ac81cfc92001d92
Ashray <ashray.velapanur@gmail.com>
2015-03-30 15:13:26 +0530

    added endpoint to get all bouts

--- src/handlers/web/bouts.py
+++ src/handlers/web/bouts.py
@@ -12,12 +12,15 @@ from model.bout import Bout
 from model.photo import Photo
 
 class CreateBoutHandler(webapp2.RequestHandler):
+	def create_bout(self, user, name, period):
+		Bout(owner=user, name=name, period=period).put()
+
 	def post(self):
 		name = self.request.get('name')
 		period = self.request.get('period')
 		email = self.request.get('email')
 		user = User.get_by_key_name(email)
-		Bout(parent=user, name=name, period=period).put()
+		self.create_bout(user, name, period)
 
 	def get(self):
 		template_values = {}
@@ -25,20 +28,21 @@ class CreateBoutHandler(webapp2.RequestHandler):
 		self.response.out.write(template.render(path, template_values))
 
 class AddPhotoHandler(webapp2.RequestHandler):
+	def create_photo(self, bout, user,image):
+		Photo(parent=bout, user=user, image=image).put()
+
 	def get(self):
 		template_values = {}
 		path = 'templates/add_photo.html'
 		self.response.out.write(template.render(path, template_values))
 
 	def post(self):
-		#session = get_current_session()
-		#email = session['email']
 		email = self.request.get('email')
 		bout_id = long(self.request.get('bout_id'))
 		image = self.request.get('image')
 		user = User.get_by_key_name(email)
 		bout = Bout.get_by_id(bout_id)
-		Photo(parent=bout, user=user, image=image).put()
+		self.create_photo(bout, user, image)
 
 class GetPhotoHandler(webapp2.RequestHandler):
     def get(self):
@@ -46,6 +50,27 @@ class GetPhotoHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'image/png'
         self.response.out.write(photo.image)
 
+class GetBoutsHandler(webapp2.RequestHandler):
+    def get(self):
+    	response = []
+    	bouts = Bout.all()
+    	for bout in bouts:
+    		bout_json = {}
+    		bout_json['name'] = bout.name
+    		bout_json['time_left'] = bout.period
+    		bout_json['photos'] = []
+    		photos = Photo.all().ancestor(bout)
+    		for photo in photos:
+    			photo_json = {}
+    			photo_json['image'] = 'image_url'
+    			photo_json['owner'] = photo.user.name
+    			bout_json['photos'].append(photo_json)
+    		response.append(bout_json)
+    	self.response.write(json.dumps(response))
+
+
+
 application = webapp2.WSGIApplication([	('/bouts/create', CreateBoutHandler),
+										('/bouts/get', GetBoutsHandler),
 										('/bouts/add_photo', AddPhotoHandler),
 										('/bouts/get_photo', GetPhotoHandler)], debug=True)
--- src/model/bout.py
+++ src/model/bout.py
@@ -2,5 +2,6 @@ from google.appengine.ext import db
 
 class Bout(db.Model):
     name = db.StringProperty(indexed=False)
+    owner = db.ReferenceProperty(indexed=False)
     period = db.StringProperty(indexed=False)
 
