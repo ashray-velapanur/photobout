df6174c5c0da62d88a0958795de359722e9cadc1
Ashray <ashray.velapanur@gmail.com>
2015-03-30 11:32:29 +0530

    handling facebook login

--- src/handlers/web/users.py
+++ src/handlers/web/users.py
@@ -1,9 +1,13 @@
 import webapp2
+import json
 from webapp2_extras.security import generate_password_hash, check_password_hash
 
 from gaesessions import get_current_session, set_current_session
 
+from google.appengine.api import urlfetch
+
 from model.user import User
+from model.third_party_user import ThirdPartyUser
 from config import PEPPER
 
 class SignupHandler(webapp2.RequestHandler):
@@ -18,7 +22,7 @@ class SignupHandler(webapp2.RequestHandler):
         confirm_password = self.request.get('confirm_password')
         self.create_user(email, name, password)
 
-class CustomLoginHandler(webapp2.RequestHandler):
+class LoginHandler(webapp2.RequestHandler):
     def check_password(self, email, password):
         password_hash = User.get_by_key_name(email).password
         return check_password_hash(password, password_hash, pepper=PEPPER)
@@ -27,11 +31,24 @@ class CustomLoginHandler(webapp2.RequestHandler):
         session = get_current_session()
         session['email'] = email
 
-    def get(self):
+    def handle_custom_login(self):
         email = self.request.get('email')
         password = self.request.get('password')
         if self.check_password(email, password):
             self.set_session(email)
+
+    def handle_facebook_login(self):
+        access_token = self.request.get('access_token')
+        user_id = self.request.get('user_id')
+        profile_url = 'https://graph.facebook.com/me?access_token=%s'
+        response = json.loads(urlfetch.fetch(profile_url%access_token).content)
+        self.response.write(response)
+
+    def get(self, network):
+        if network == 'custom':
+            self.handle_custom_login()
+        elif network == 'facebook':
+            self.handle_facebook_login()
 
 class CheckSessionHandler(webapp2.RequestHandler):
     def get(self):
@@ -39,5 +56,6 @@ class CheckSessionHandler(webapp2.RequestHandler):
         self.response.write(session['email'] if session.has_key('email') else 'no key')
 
 application = webapp2.WSGIApplication([('/users/signup', SignupHandler),
-                                       ('/users/custom/login', CustomLoginHandler),
+                                       #('/users/custom/login', CustomLoginHandler),
+                                       ('/users/([^/]+)/login', LoginHandler),
                                        ('/users/checksession', CheckSessionHandler)], debug=True)
