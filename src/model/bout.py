064a4a48edd76c115be351f90c6277e6b038c7aa
Ashray <ashray.velapanur@gmail.com>
2015-04-13 11:55:21 +0530

    added created at field to bouts

--- src/model/bout.py
+++ src/model/bout.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from google.appengine.ext import db
 
 from flufl import enum
@@ -13,12 +15,13 @@ class Bout(db.Model):
     name = db.StringProperty(indexed=False)
     description = db.TextProperty(indexed=False)
     owner = db.ReferenceProperty(indexed=False)
-    period = db.StringProperty(indexed=False)
+    created_at = db.DateTimeProperty(indexed=False)
+    period = db.IntegerProperty(indexed=False)
     permission = EnumProperty(Permission)
 
     @classmethod
     def create(cls, user, name, description, period, permission):
-        bout = Bout(owner=user, name=name, description=description, period=period, permission=int(permission))
+        bout = Bout(owner=user, name=name, description=description, period=int(period), permission=int(permission), created_at=datetime.now())
         bout.put()
         return bout
 
@@ -33,3 +36,4 @@ class Bout(db.Model):
     @property
     def photos(self):
         return Photo.all().ancestor(self).fetch(None)
+
