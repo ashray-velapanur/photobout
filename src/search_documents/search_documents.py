bc30e23b1c8af1be76906d4161377ac5c818d474
Ashray <ashray.velapanur@gmail.com>
2015-04-16 13:46:33 +0530

    added allowed fields to search docs

--- src/search_documents/search_documents.py
+++ src/search_documents/search_documents.py
@@ -1,19 +1,26 @@
 from google.appengine.api import search
 
 class SearchDocument(object):
-	def __init__(self, index_name, id, kwargs):
+	def __init__(self, index_name, id, fields):
 		self.index = search.Index(name=index_name)
-		self.document = self.make_document(id, kwargs)
+		self.document = self.make_document(id, fields)
 
-	def make_document(self, id, kwargs):
-		fields = [search.TextField(name=name, value=value) for name, value in kwargs.iteritems()]
+	def make_document(self, id, fields):
 		document = search.Document(doc_id=id, fields=fields)
 		return document
 
 	def save(self):
 		self.index.put(self.document)
 
+	def make_fields(self, kwargs):
+		fields = []
+		for name, value in kwargs.iteritems():
+			if name in self._fields:
+				fields.append(self._fields[name](name=name, value=value))
+		return fields
+
 class BoutDocument(SearchDocument):
 	def __init__(self, id, **kwargs):
-		super(BoutDocument, self).__init__('bouts', id, kwargs)
+		self._fields = {'name': search.TextField, 'description': search.TextField}
+		super(BoutDocument, self).__init__('bouts', id, self.make_fields(kwargs))
 
