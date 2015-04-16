1f735fcc61c739547fc420bfaa1736cdf1a25155
Ashray <ashray.velapanur@gmail.com>
2015-04-16 16:36:23 +0530

    added document search functionality

--- src/search_documents/search_documents.py
+++ src/search_documents/search_documents.py
@@ -13,11 +13,23 @@ class SearchDocument(object):
         for name, value in kwargs.iteritems():
             if name in self.fields:
                 fields.append(self.fields[name](name=name, value=value))
+            fields.append()
         return fields
 
     def create(self, id, **kwargs):
         self.make_document(id, self.make_fields(kwargs))
 
+    def search(self, string):
+        results = self.index.search(string).results
+        response = []
+        for result in results:
+            result_dict = {}
+            result_dict['id'] = result.doc_id
+            result_dict['fields'] = {}
+            for field in result.fields:
+                result_dict['fields'][field.name] = field.value
+            response.append(result_dict)
+        return response
 
 class BoutDocument(SearchDocument):
     def __init__(self):
@@ -25,6 +37,4 @@ class BoutDocument(SearchDocument):
         self.fields = {'name': search.TextField, 'description': search.TextField}
         super(BoutDocument, self).__init__(self.index_name)
 
-#    @classmethod
-#    def search(cls):
 
