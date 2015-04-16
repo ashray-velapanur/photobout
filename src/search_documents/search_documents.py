1130e5130efc2298196f597ffcb3fefe735eb51b
Ashray <ashray.velapanur@gmail.com>
2015-04-16 15:31:00 +0530

    cleaned up search docs code

--- src/search_documents/search_documents.py
+++ src/search_documents/search_documents.py
@@ -1,26 +1,30 @@
 from google.appengine.api import search
 
 class SearchDocument(object):
-    def __init__(self, index_name, id, fields):
+    def __init__(self, index_name):
         self.index = search.Index(name=index_name)
-        self.document = self.make_document(id, fields)
 
     def make_document(self, id, fields):
         document = search.Document(doc_id=id, fields=fields)
-        return document
-
-    def put(self):
-        self.index.put(self.document)
+        self.index.put(document)
 
     def make_fields(self, kwargs):
         fields = []
         for name, value in kwargs.iteritems():
-            if name in self._fields:
-                fields.append(self._fields[name](name=name, value=value))
+            if name in self.fields:
+                fields.append(self.fields[name](name=name, value=value))
         return fields
 
+    def create(self, id, **kwargs):
+        self.make_document(id, self.make_fields(kwargs))
+
+
 class BoutDocument(SearchDocument):
-    def __init__(self, id, **kwargs):
+    def __init__(self):
         self.index_name = 'bouts'
-        self._fields = {'name': search.TextField, 'description': search.TextField}
-        super(BoutDocument, self).__init__(self.index_name, id, self.make_fields(kwargs))
+        self.fields = {'name': search.TextField, 'description': search.TextField}
+        super(BoutDocument, self).__init__(self.index_name)
+
+#    @classmethod
+#    def search(cls):
+
