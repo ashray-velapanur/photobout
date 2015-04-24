791d8e0b274b1a711c72750cf1cb9edb03519dd8
Ashray <ashray.velapanur@gmail.com>
2015-04-24 11:50:01 +0530

    saving notification type in db

--- src/model/notification.py
+++ src/model/notification.py
@@ -2,16 +2,11 @@ import datetime
 
 from google.appengine.ext import db
 
-MESSAGES = {
-    'invited': "%s invited you to %s."
-}
-
 class Notification(db.Model):
-    message = db.StringProperty(indexed=False)
+    notification_type = db.StringProperty(indexed=False)
     viewed = db.BooleanProperty(indexed=False)
     timestamp = db.DateTimeProperty(indexed=False)
 
     @classmethod
-    def create(cls, type, user, *args):
-        message = MESSAGES[type]%args
-        cls(parent=user, message=message, viewed=False, timestamp=datetime.datetime.now()).put()
+    def create(cls, type, user):
+        cls(parent=user, notification_type=type, viewed=False, timestamp=datetime.datetime.now()).put()
