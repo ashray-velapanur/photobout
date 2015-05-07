ed06e2a2af5bb147f2ba0f1565e3cb253eb2da3b
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-05-07 14:23:03 +0530

    Adding followers to test data

--- src/setup_test_data.py
+++ src/setup_test_data.py
@@ -7,27 +7,30 @@ from model.photo import Photo
 from model.vote import Vote
 from model.comment import Comment
 from model.notification import Notification
+from model.following import Following
+from model.follower import Follower
 
 def setup():
-	user_1 = User.create('email1', 'firstname1', 'lastname1', 'password1')
-	tp_user_1 = ThirdPartyUser(key_name='FB', parent=user_1)
-	tp_user_1.network_id = 'fb_id_1'
-	tp_user_1.put()
-	user_2 = User.create('email2', 'firstname2', 'lasstname2', 'password2')
-	tp_user_2 = ThirdPartyUser(key_name='FB', parent=user_2)
-	tp_user_2.network_id = 'fb_id_2'
-	tp_user_2.put()
-	bout_1 = Bout.create(user_1, 'bout1', 'desc1', 1, 1)
-	photo_1 = Photo.create(bout_1, user_1, 'image_blob_key_1')
-	photo_2 = Photo.create(bout_1, user_2, 'image_blob_key_2')
-	Vote.create('email1', photo_1)
-	Vote.create('email2', photo_1)
-	Vote.create('email2', photo_2)
-	#photo_add, photo_vote, comment_add, winner, invited
-	Notification.create('photo_add', bout_1.owner, user_2.email, bout_1)
-	Notification.create('photo_vote', bout_1.owner, user_2.email, bout_1)
-	Notification.create('comment_add', bout_1.owner, user_2.email, bout_1)
-	Notification.create('winner', user_1, user_2.email, bout_1)
-	Notification.create('invited', user_1, user_2.email, bout_1)
-	Comment(parent=bout_1, user=user_1, message='message', timestamp=datetime.datetime.now()).put()
-	
+    user_1 = User.create('email1', 'firstname1', 'lastname1', 'password1')
+    tp_user_1 = ThirdPartyUser(key_name='FB', parent=user_1)
+    tp_user_1.network_id = 'fb_id_1'
+    tp_user_1.put()
+    user_2 = User.create('email2', 'firstname2', 'lasstname2', 'password2')
+    tp_user_2 = ThirdPartyUser(key_name='FB', parent=user_2)
+    tp_user_2.network_id = 'fb_id_2'
+    tp_user_2.put()
+    bout_1 = Bout.create(user_1, 'bout1', 'desc1', 1, 1)
+    photo_1 = Photo.create(bout_1, user_1, 'image_blob_key_1')
+    photo_2 = Photo.create(bout_1, user_2, 'image_blob_key_2')
+    Vote.create('email1', photo_1)
+    Vote.create('email2', photo_1)
+    Vote.create('email2', photo_2)
+    #photo_add, photo_vote, comment_add, winner, invited
+    Notification.create('photo_add', bout_1.owner, user_2.email, bout_1)
+    Notification.create('photo_vote', bout_1.owner, user_2.email, bout_1)
+    Notification.create('comment_add', bout_1.owner, user_2.email, bout_1)
+    Notification.create('winner', user_1, user_2.email, bout_1)
+    Notification.create('invited', user_1, user_2.email, bout_1)
+    Comment(parent=bout_1, user=user_1, message='message', timestamp=datetime.datetime.now()).put()
+    Follower.create('email1', user_2)
+    Following.create(user_1, 'email2')
