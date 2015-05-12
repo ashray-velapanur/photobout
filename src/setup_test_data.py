4406ec82511eee06bf8df06709ea57680bf911bb
Ashray <ashray.velapanur@gmail.com>
2015-05-12 12:10:22 +0530

    updated test data script

--- src/setup_test_data.py
+++ src/setup_test_data.py
@@ -13,15 +13,16 @@ from model.follower import Follower
 def setup():
     user_1 = User.create('email1', 'firstname1', 'lastname1', 'password1')
     tp_user_1 = ThirdPartyUser(key_name='FB', parent=user_1)
-    tp_user_1.network_id = 'fb_id_1'
+    tp_user_1.network_id = '359059317608175'
     tp_user_1.put()
     user_2 = User.create('email2', 'firstname2', 'lasstname2', 'password2')
     tp_user_2 = ThirdPartyUser(key_name='FB', parent=user_2)
-    tp_user_2.network_id = 'fb_id_2'
+    tp_user_2.network_id = '359059317608175'
     tp_user_2.put()
     bout_1 = Bout.create(user_1, 'bout1', 'desc1', 1, 1)
     photo_1 = Photo.create(bout_1, user_1, 'image_blob_key_1')
     photo_2 = Photo.create(bout_1, user_2, 'image_blob_key_2')
+    bout_2 = Bout.create(user_2, 'bout2', 'desc2', 1, 2)
     Vote.create('email1', photo_1)
     Vote.create('email2', photo_1)
     Vote.create('email2', photo_2)
