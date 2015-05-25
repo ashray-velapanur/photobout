ecbf1260edc6700202f15a47951e409e34ce7d7d
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-05-25 17:17:01 +0530

    Loading bout from avtivity screen only when tapped

--- src/handlers/web/users.py
+++ src/handlers/web/users.py
@@ -173,7 +173,7 @@ def make_notification_dict(params):
     notification_dict['timestamp'] = notification.formatted_timestamp
     notification_dict['profile_picture'] = from_user.profile_picture
     notification_dict['from_name'] = 'You' if notification_type == 'winner' else from_user.name
-    notification_dict['bout'] = util.make_bout_dict(bout, notification.user.email)
+    notification_dict['bout'] = bout.id
     notification_dict['message'] = notification.message
     return notification_dict
 
